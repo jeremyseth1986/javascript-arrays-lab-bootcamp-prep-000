9a4b270b6ef7f46146786cf159501b6cf0cc1c2b
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-03-25 11:50:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
-  
+  var kittens2 = ["Silas", "Cabron", "Felix"];
+  return kittens.concat(kittens2);
 }
