3f5f813cbd7f90300df009388618cd0cc4e2a12b
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-03-25 12:10:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ return kittens.push(name);
 function destructivelyPrependKitten(name) {
   return kittens.unshift(name);
 }
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
